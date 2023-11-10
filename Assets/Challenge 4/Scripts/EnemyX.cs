61450d37cf4ac54c2686d78f817b8ef8644e1ada
Unknown2k10 <125458085+Unknown2k10@users.noreply.github.com>
2023-11-10 21:18:53 +0000

    Add files via upload

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    public GameObject playerGoal; // Assign the player goal GameObject through the Unity Inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+
+        // Check if playerGoal is not assigned and log an error if that's the case
+       // if (playerGoal == null)
+        //{
+          //  Debug.LogError("Player Goal is not assigned. Please assign it in the Inspector.");
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if playerGoal is not assigned or if it doesn't have a Rigidbody
+        if (playerGoal == null || !playerGoal.TryGetComponent<Rigidbody>(out Rigidbody playerRb))
+        {
+            return;
+        }
+
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        playerRb.AddForce(lookDirection * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 2;
+    private GameObject focalPoint;
+
+    public ParticleSystem smoke;
+    public bool turboAvailable = false; // Initialize as false
+    public float turboDuration = 2.0f;
+    private float turboSpeedMultiplier = 7f;
+    public bool hasPowerup = false; // Initialize as false
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+    public int turboWait = 5;
+    private float powerupStrength = 25; // Assuming this is used elsewhere
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        // Initially, the power-up indicator should be deactivated
+        powerupIndicator.SetActive(false);
+    }
+
+    void Update()
+    {
+        MoveForward();
+        UpdatePowerupIndicatorPosition(); // Update the power-up indicator position in the Update method
+        UpdateSmokePosition(); // Update the smoke position in the Update method
+
+        // Check for power-up activation (e.g., when the player presses a key)
+        if (Input.GetKeyDown(KeyCode.P) && !hasPowerup && turboAvailable)
+        {
+            Debug.Log("Power-up activation key pressed!"); // Debug statement for testing
+            ActivatePowerup();
+        }
+    }
+
+    private void MoveForward()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (playerRb != null)
+        {
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.Space) && turboAvailable)
+            {
+                playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput * turboSpeedMultiplier, ForceMode.Impulse);
+                StartTurboCooldown();
+                smoke.Play();
+            }
+            else
+            {
+                playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
+            }
+        }
+    }
+
+    private void UpdatePowerupIndicatorPosition()
+    {
+        // Update the position of the power-up indicator relative to the player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+    }
+
+    private void UpdateSmokePosition()
+    {
+        // Update the position of the smoke particle system relative to the player
+        smoke.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+    }
+
+    private void ActivatePowerup()
+    {
+        hasPowerup = true;
+        powerupIndicator.SetActive(true); // Activate the power-up indicator
+        StartCoroutine(PowerupCooldown());
+    }
+
+    // Method to start the turbo cooldown
+    private void StartTurboCooldown()
+    {
+        turboAvailable = false; // Disable turbo while it's on cooldown
+        StartCoroutine(TurboCooldown());
+    }
+
+    // Coroutine to count down turbo cooldown duration
+    IEnumerator TurboCooldown()
+    {
+        yield return new WaitForSeconds(turboDuration);
+        turboAvailable = true; // Turbo is available again
+    }
+
+    // Coroutine to count down power-up duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false); // Deactivate the power-up indicator when the power-up expires
+    }
+
+    // OnTriggerEnter is used for power-up collection
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            Debug.Log("Power-up collected!"); // Debug statement for testing
+
+            // Enable turbo when the power-up is collected
+            turboAvailable = true;
+            powerupIndicator.SetActive(true); // Activate the power-up indicator
+
+            // Deactivate the collected power-up object (power-up clone)
+            other.gameObject.SetActive(false);
+
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // OnCollisionEnter is used for collisions with enemies
+    private void OnCollisionEnter(Collision other)
+    {
+        if (hasPowerup && other.gameObject.CompareTag("Enemy"))
+        {
+            // Handle collision with enemies when the power-up is active
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+}
